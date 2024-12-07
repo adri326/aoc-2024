00387d0fa4865373ab9902a64d8db219f5f5c84e
Emilie Burgun <emilie@shadamethyst.xyz>
2024-12-07 12:56:06 +0100

    Solve day 7 in ocaml

--- src/day7.ml
+++ src/day7.ml
@@ -5,9 +5,8 @@ type unsolved = {
 
 type operator =
   | Add
-  (* | Sub *)
   | Mul
-  (* | Div *)
+  | Concat
 
 let read_input chan: string list =
   let res = ref [] in
@@ -33,22 +32,43 @@ let parse_line (line: string): unsolved =
   | _ ->
     raise (Failure "Invalid line")
 
+let next_power (base: int) (value: int): int =
+  let rec find_next_power (guess: int): int =
+    if guess > value then
+      guess
+    else
+      find_next_power (guess * base)
+  in
+  find_next_power 1
 
+let option_or opt or_else =
+  match opt with
+  | Some res -> Some res
+  | None -> or_else ()
 
-let rec find_operators_sub (operands: int list) (remaining_result: int): operator list option =
+let rec find_operators_sub (operands: int list) (remaining_result: int) (allow_concat: bool): operator list option =
   let try_mul (head: int) (rest: int list) (remaining_result: int): operator list option =
     if remaining_result mod head == 0 then
-      match find_operators_sub rest (remaining_result / head) with
-        | Some ops -> Some (Mul :: ops)
-        | None -> None
+      match find_operators_sub rest (remaining_result / head) allow_concat with
+      | Some ops -> Some (Mul :: ops)
+      | None -> None
     else
       None
   in
   let try_add (head: int) (rest: int list) (remaining_result: int): operator list option =
     if remaining_result - head >= 0 then
-      match find_operators_sub rest (remaining_result - head) with
-        | Some ops -> Some (Add :: ops)
-        | None -> None
+      match find_operators_sub rest (remaining_result - head) allow_concat with
+      | Some ops -> Some (Add :: ops)
+      | None -> None
+    else
+      None
+  in
+  let try_concat (head: int) (rest: int list) (remaining_result: int): operator list option =
+    let ten_power = next_power 10 head in
+    if allow_concat && remaining_result mod ten_power == head then
+      match find_operators_sub rest ((remaining_result - head) / ten_power) allow_concat with
+      | Some ops -> Some (Concat :: ops)
+      | None -> None
     else
       None
   in
@@ -57,30 +77,40 @@ let rec find_operators_sub (operands: int list) (remaining_result: int): operato
   | [] -> if remaining_result == 0 then Some [] else None
   | [head] -> if remaining_result == head then Some [] else None
   | head :: rest ->
-    match try_mul head rest remaining_result with
-    | Some res -> Some res
-    | None ->
-      try_add head rest remaining_result
+    option_or (try_mul head rest remaining_result)
+      (fun () ->
+        option_or (try_add head rest remaining_result)
+        (fun () ->
+          try_concat head rest remaining_result
+        )
+      )
 
-let find_operators (line: unsolved): operator list option =
-  find_operators_sub (List.rev line.operands) line.result
+let find_operators (line: unsolved) (allow_concat: bool): operator list option =
+  find_operators_sub (List.rev line.operands) line.result allow_concat
 
 let _print_operator (op: operator): unit = match op with
   | Add -> print_string "+"
   | Mul -> print_string "*"
-
+  | Concat -> print_string "|"
 
 let day_file = "./input/day7.txt"
 let () =
   let input = read_input (open_in day_file) in
   let input = List.map parse_line (List.filter (fun x -> String.length x > 0) input) in
   (* List.iter (fun line ->
-    match find_operators line with
+    match find_operators line true with
     | Some ops -> List.iter (fun op -> _print_operator op) ops; print_newline ()
-    | None -> print_string "Nope"; print_newline ()
+    | None -> print_string "!"; print_newline ()
   ) input; *)
   print_int (List.fold_left (fun sum line ->
-    if Option.is_some (find_operators line) then
+    if Option.is_some (find_operators line false) then
+      sum + line.result
+    else
+      sum
+  ) 0 input);
+  print_newline ();
+  print_int (List.fold_left (fun sum line ->
+    if Option.is_some (find_operators line true) then
       sum + line.result
     else
       sum
